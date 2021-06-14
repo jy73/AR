9e9e1bfe2b54b647bbd4b8f71a0b3c86af52060c
Jordan Yee <jordantyee@berkeley.edu>
2021-06-14 16:08:02 -0700

    added in functionality for glasses raise by eyebrows

--- TestAR/ContentView.swift
+++ TestAR/ContentView.swift
@@ -12,6 +12,8 @@ import Swift
 
 var arView: ARView!
 var sunglasses: Experience.FilterOne!
+var glasses:Experience.Glasses!
+var filterVal = 0
 var rimL: Entity!
 var rimR: Entity!
 var stickL: Entity!
@@ -20,6 +22,8 @@ var middle: Entity!
 var rimLModel: ModelEntity!
 var rimRModel: ModelEntity!
 var middleModel: ModelEntity!
+
+var glassesObj: Entity!
 
 struct ContentView : View {
     @State var filterNumber: Int = 0
@@ -33,6 +37,7 @@ struct ContentView : View {
                 
                 Button(action: {
                     self.filterNumber = 0
+                    filterVal = 0
                 }) {
                     Text("Sunglasses")
                         .padding()
@@ -45,6 +50,7 @@ struct ContentView : View {
                 
                 Button(action: {
                     self.filterNumber = 1
+                    filterVal = 1
                 }) {
                     Text("Hello World")
                         .padding()
@@ -57,6 +63,7 @@ struct ContentView : View {
                 
                 Button(action: {
                     self.filterNumber = 2
+                    filterVal = 2
                 }) {
                     Text("Glasses")
                         .padding()
@@ -87,6 +94,7 @@ struct ARViewContainer: UIViewRepresentable {
     func updateUIView(_ uiView: ARView, context: Context) {
         
         sunglasses = nil
+        glasses = nil
         arView.scene.anchors.removeAll()
         
         let config = ARFaceTrackingConfiguration()
@@ -116,6 +124,8 @@ struct ARViewContainer: UIViewRepresentable {
         case 2:
             let faceAnchor = try! Experience.loadGlasses()
             uiView.scene.anchors.append(faceAnchor)
+            glasses = faceAnchor
+            glassesObj = glasses.findEntity(named: "glassesObj")
             break
         default:
             break
@@ -137,29 +147,64 @@ struct ARViewContainer: UIViewRepresentable {
         
         func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
             
-            guard sunglasses != nil else {return}
+            if filterVal == 0 {
+                guard sunglasses != nil else {return}
+                
+                var faceAnchor: ARFaceAnchor?
             
-            var faceAnchor: ARFaceAnchor?
-        
-            
-            for anchor in anchors {
-                if let a = anchor as? ARFaceAnchor {
-                    faceAnchor = a
+                
+                for anchor in anchors {
+                    if let a = anchor as? ARFaceAnchor {
+                        faceAnchor = a
+                    }
                 }
+                
+                guard let blendShapes = faceAnchor?.blendShapes,
+                let jawVal = blendShapes[.jawOpen]?.floatValue,
+                let smileLVal = blendShapes[.mouthSmileLeft]?.floatValue,
+                let smileRVal = blendShapes[.mouthSmileRight]?.floatValue else {return}
+                
+
+                rimL.scale.z = jawVal * 2 + 1.0
+                rimR.scale.z = jawVal * 2 + 1.0
+                
+                rimLModel.model?.materials = [SimpleMaterial.init(color: .init(red: CGFloat(min(smileLVal, smileRVal)) + 0.24, green: 0.0, blue: 0.0, alpha: 1), isMetallic: true)]
+                rimRModel.model?.materials = [SimpleMaterial.init(color: .init(red: CGFloat(min(smileLVal, smileRVal)) + 0.25, green: 0.0, blue: 0.0, alpha: 1), isMetallic: true)]
+                middleModel.model?.materials = [SimpleMaterial.init(color: .init(red: CGFloat(min(smileLVal, smileRVal)) + 0.25, green: 0.0, blue: 0.0, alpha: 1), isMetallic: true)]
+            } else if filterVal == 2 {
+                guard glasses != nil else {return}
+                
+                var faceAnchor: ARFaceAnchor?
+                
+                for anchor in anchors {
+                    if let a = anchor as? ARFaceAnchor {
+                        faceAnchor = a
+                    }
+                }
+                
+                guard let blendShapes = faceAnchor?.blendShapes,
+                      let leftBrow = blendShapes[.browOuterUpLeft]?.floatValue,
+                      let rightBrow = blendShapes[.browOuterUpRight]?.floatValue else {return}
+                
+//                let radians = (jawVal * 45.0 * Float.pi / 180.0) + 0
+//                glassesObj.setOrientation(simd_quatf(angle: -0.785398, axis: SIMD3<Float>(0,1,0)), relativeTo: nil)
+                let position = min(leftBrow, rightBrow) * -0.1 + 0.0144
+                glassesObj.position.z = position
+//                let y = 6.0 * jawVal + 1.44
+//                glassesObj.setPosition(SIMD3<Float>(0, 0, -4.56), relativeTo: nil)
+//                glassesObj.transform.translation = SIMD3<Float>(0, 0, -4.56)
+//                glasses.glassesObj?.orientation = simd_quatf(
+//                    angle: convToRad(-100 + (60)),
+//                    axis: [1, 0, 0])
+
+
             }
             
-            guard let blendShapes = faceAnchor?.blendShapes,
-            let jawVal = blendShapes[.jawOpen]?.floatValue,
-            let smileLVal = blendShapes[.mouthSmileLeft]?.floatValue,
-            let smileRVal = blendShapes[.mouthSmileRight]?.floatValue else {return}
+            func convToRad(_ value: Float) -> Float {
+                  return value * .pi / 180
+                }
             
 
-            rimL.scale.z = jawVal * 2 + 1.0
-            rimR.scale.z = jawVal * 2 + 1.0
-            
-            rimLModel.model?.materials = [SimpleMaterial.init(color: .init(red: CGFloat(min(smileLVal, smileRVal)) + 0.24, green: 0.0, blue: 0.0, alpha: 1), isMetallic: true)]
-            rimRModel.model?.materials = [SimpleMaterial.init(color: .init(red: CGFloat(min(smileLVal, smileRVal)) + 0.25, green: 0.0, blue: 0.0, alpha: 1), isMetallic: true)]
-            middleModel.model?.materials = [SimpleMaterial.init(color: .init(red: CGFloat(min(smileLVal, smileRVal)) + 0.25, green: 0.0, blue: 0.0, alpha: 1), isMetallic: true)]
 
         }
     }
